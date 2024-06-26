a8f26be765b5b245a33dc49e9af6e200f3b116b1
EnzoNeve5 <111204969+EnzoNeve5@users.noreply.github.com>
2024-06-26 14:11:30 -0300

    Add files via upload

---
+++ unidades-de-área.py
@@ -0,0 +1,217 @@
+n1 = float(input ('Digite um valor em \033[32mm²\033[m: '))
+a = n1
+b = n1 * 100
+c = n1 * 10000
+d = n1 * 1000000
+e = n1 * 10000000000000000000000000000
+f = n1 / 1000000
+g = n1 / 10000
+h = n1 / 1000
+i = n1 / 100
+
+j = n1 * 1073 / 100000000000
+k = n1 * 3861 / 10000000000
+l = n1 * 1544 / 1000000000
+m = n1 * 2471 / 10000000
+n = n1 * 9884 / 10000000
+o = n1 * 3954 / 100000
+p = n1 * 3954 / 100000
+q = n1 * 1196 / 1000
+r = n1 * 1076 / 100
+s = n1 * 1550
+t = n1 * 1550003100
+
+u = n1 * 3025 / 100
+v = n1 * 3025 / 1000
+w = n1 * 605 / 1000
+x = n1 * 3025 / 10000
+y = n1 * 3025 / 10000
+z = n1 * 1008 / 100000
+aa = n1 * 1008 / 1000000
+ab = n1 * 1008 / 10000000
+
+ac = n1 * 15 / 1000000
+ad = n1 * 15 / 10000
+ae = n1 * 15 / 1000
+af = n1 * 9 / 100
+ag = n1 * 15 / 100
+ah = n1 * 15 / 10
+ai = n1 * 9
+aj = n1 * 900
+
+ak = n1 * 1628 / 100000000
+al = n1 * 1628 / 1000000
+am = n1 * 1628 / 100000
+an = n1 * 9766 / 100000
+ao = n1 * 1628 / 10000
+ap = n1 * 1628 / 1000
+aq = n1 * 9766 / 1000
+ar = n1 * 9766 / 10
+
+ba = n1 * 625 / 1000000
+bb = n1 * 25 / 10000
+bc = n1 * 25 / 100
+
+bd = n1 * 1053 / 1000000
+be = n1 * 4211 / 1000000
+bf = n1 * 6316 / 1000000
+bg = n1 * 1263 / 100000
+bh = n1 * 2437 / 100000
+bi = n1 * 2924 / 100000
+bj = n1 * 1053 / 10000
+bk = n1 * 2924 / 10000
+bl = n1 * 4211 / 10000
+bm = n1 * 1053 / 100
+
+bn = n1 * 4249 / 10000000000
+bo = n1 * 17 / 10000000
+bp = n1 * 17 / 100000
+bq = n1 * 3399 / 10000000
+br = n1 * 6799 / 10000000
+bs = n1 * 272 / 100000
+bt = n1 * 2448 / 100000
+bu = n1 * 979 / 10000
+bv = n1 * 979 / 100
+bw = n1 * 114 / 10
+
+bx = n1 * 2925 / 10000000
+by = n1 * 2925 / 100000
+bz = n1 * 2632 / 10000
+ca = n1 * 9477 / 1000
+cb = n1 * 1365 / 1000
+
+cc = n1 * 5725 / 100000000000
+cd = n1 * 1431 / 1000000000
+ce = n1 * 2588 / 1000000000
+cf = n1 * 3106 / 1000000000
+cg = n1 * 1553 / 10000000
+ch = n1 * 2236 / 10000000
+ci = n1 * 2544 / 10000000
+cj = n1 * 1863 / 1000000
+ck = n1 * 7454 / 1000000
+cl = n1 * 8945 / 100000
+cm = n1 * 1431 / 1000
+cn = n1 * 1288 / 100
+
+co = n1 * 2066 / 10000
+cp = n1 * 8264 / 10000
+cq = n1 * 2066 / 100
+cr = n1 * 1322
+
+cs = n1 * 9153 / 100000000
+ct = n1 * 6865 / 100000000
+cu = n1 * 8787 / 10000000000
+cv = n1 * 1977 / 1000
+cw = n1 * 1176 / 100
+
+cx = n1 / 1000
+cy = n1 / 1000
+cz = n1 * 2446 / 1000000
+da = n1 / 1000
+db = n1 / 1000
+dc = n1 * 7874 / 10000000
+dd = n1 * 4 / 10000
+de = n1 / 1000
+df = n1 * 1088 / 1000000
+dg = n1 * 2381 / 10000000
+dh = n1 * 5714 / 1000000
+di = n1 / 10000
+dj = n1 * 1197 / 10000000
+dk = n1 * 1431 / 10000000
+dl = n1 * 1417 / 10000000
+dm = n1 * 1434 / 10000000
+dn = n1 * 1429 / 10000000
+do = n1 * 5928 / 100000000
+dp = n1 * 6359 / 100000000
+dq = n1 * 1417 / 10000000
+dr = n1 * 1333 / 10000000
+ds = n1 / 10000
+dt = n1 * 1355 / 10000000
+du = n1 * 1563 / 1000
+dv = n1 * 1417 / 1000
+dw = n1 * 1391 / 1000
+dx = n1 * 1336 / 1000
+dy = n1 * 8264 / 10000
+
+dz = n1 * 1974
+ea = n1 * 1973525
+eb = n1 * 1973525241
+
+ec = n1 / 1000000000000000000000000000000000000000000000000000000000000000000000000 * 3828
+
+print ('-' * 30)
+print ('\033[0;30;46m-------MEDIDAS MÉTRICAS-------\033[m')
+print ('-' * 30)
+print ('\033[36m{} m² |{} dm² |{} cm² |{} mm² |{} b |{} km² |{} hectare |{} decare |{} a |\033[m'.format(a, b, c, d, e, f, g, h, i))
+
+print ('-' * 30)
+print ('\033[0;30;46m-------EUA E INGLATERRA-------\033[m')
+print ('-' * 30)
+print ('\033[36m{} township |{} milha quadrada |{} homestead |{} acre |{} rood |{} rod quadrado |{} perch |{} yd² |{} ft² |{} in² |{} th² |\033[m'.format(j, k, l, m, n, o, p, q, r, s, t))
+
+print ('-' * 29)
+print ('\033[0;30;46m------------JAPÃO------------\033[m')
+print ('-' * 29)
+print ('\033[36m{} shaku |{} go |{} jo |{} tsubo |{} bu |{} se |{} tan |{} cho |\033[m'.format(u, v, w, x, y, z, aa, ab))
+
+print ('-' * 29)
+print ('\033[0;30;46m-------CHINA 1915#1930-------\033[m')
+print ('-' * 29)
+print ('\033[36m{} qing |{} mu |{} fen |{} fang zhang |{} li - 㢆 ou 釐 |{} hao |{} fang chi |{} fang cun |\033[m'.format(ac, ad, ae, af, ag, ah, ai, aj))
+
+print ('-' * 29)
+print ('\033[0;30;46m-------CHINA 1930#HOJE-------\033[m')
+print ('-' * 29)
+print ('\033[36m{} qing |{} mu |{} fen |{} fang zhang |{} li - 㢆 ou 釐 |{} hao |{} fang chi |{} fang cun |\033[m'.format(ak, al, am, an, ao, ap, aq, ar))
+
+print ('-' * 29)
+print ('\033[0;30;46m----------TAILÂNDIA----------\033[m')
+print ('-' * 29)
+print ('\033[36m{} rai |{} ngan |{} tarang wah |\033[m'.format(ba, bb, bc))
+
+print ('-' * 30)
+print ('\033[0;30;43m------------GRÉCIA------------\033[m')
+print ('-' * 30)
+print ('\033[34m{} plethron |{} aroura |{} hektos |{} hēmiektos |{} dodecaorgion |{} decaorgion |{} akaina |{} hexapodēs ou orgya |{} bema ou diploum |{} pous |\033[m'.format(bd, be, bf, bg, bh, bi, bj, bk, bl, bm))
+
+print ('-' * 30)
+print ('\033[0;30;43m-------------ROMA-------------\033[m')
+print ('-' * 30)
+print ('\033[34m{} saltus |{} centurium |{} geredium |{} uger |{} akt quadrado |{} klima |{} akt quadrado pequeno |{} decimpeda quadrada |{} ped quadrado comum |{} ped quadrado legal |\033[m'.format(bn, bo, bp, bq, br, bs, bt, bu, bv, bw))
+
+print ('-' * 30)
+print ('\033[0;30;43m--------FRANCÊS ANTIGO--------\033[m')
+print ('-' * 30)
+print ('\033[34m{} arpent |{} perche |{} toise quadrada |{} pied |{} pouce |\033[m'.format(bx, by, bz, ca, cb))
+
+print ('-' * 30)
+print ('\033[0;30;43m--------ESPANHA ANTIGA--------\033[m')
+print ('-' * 30)
+print ('\033[34m{} legua |{} labor |{} caballería |{} yugada |{} fanegada |{} aranzada |{} cuerda de Puerto Rico |{} celemin |{} cuartillo |{} estadal cuadrado |{} vara cuadrada |{} pie cuadrado |\033[m'.format(cc, cd, ce, cf, cg, ch, ci, cj, ck, cl, cm, cn))
+
+print ('-' * 29)
+print ('\033[0;30;43m-------PORTUGAL ANTIGO-------\033[m')
+print ('-' * 29)
+print ('\033[34m{} braça quadrada |{} vara quadrada |{} palmo quadrado |{} polegada quadrada |\033[m'.format(co, cp, cq, cr))
+
+print ('-' * 29)
+print ('\033[0;30;43m--------RÚSSIA ANTIGA--------\033[m')
+print ('-' * 29)
+print ('\033[34m{} desyatina país |{} desyatina fazendeiros |{} verst quadrado |{} arshin quadrado |{} pé quadrado |\033[m'.format(cs, ct, cu, cv, cw))
+
+print ('-' * 30)
+print ('\033[0;30;46m---------AINDA EM USO---------\033[m')
+print ('-' * 30)
+print ('\033[36m{} dulum (дулум) da Bósnia, Herzegovina e Sérvia |{} dynym ou dylym da Albânia |{} donum do Chipre |{} skales (σκάλες) do Chipre |{} stremma grega |{} stremma turca da Grécia |{} dunam do Iraque |{} dunam (dönüm, دونم) da Turquia, Síria, Israel, Palestina, Jordânia, Líbano |{} dunam (dönüm, دونم) antigo (anterior a 1928) da Turquia, Síria, Israel, Palestina, Jordânia, Líbano |{} feddan (فدّان) do Egito, Sudão, Síria, Omã |{} kirat (قيراط) do Egito |{} manzana da Argentina |{} manzana da Belize |{} manzana da Costa Rica |{} manzana da Guatemala |{} manzana da Honduras |{} manzana da Nicarágua |{} cuadra da Argentina |{} cuadra do Chile |{} cuadra do Equador |{} cuadra do Paraguai |{} cuadra do Peru |{} cuadra do Uruguai |{} vara quadrada do Panamá e da Colômbia |{} vara quadrada do Chile, Costa Rica, República Dominicana, Equador, El Salvador, Guatemala, Honduras, México, Nicarágua, Peru, São Tomé e Príncipe, Venezuela |{} vara quadrada de Cuba |{} vara quadrada da Argentina, Paraguai, Uruguai |{} vara quadrada do Brasil |\033[m'.format(cx, cy, cz, da, db, dc, dd, de, df, dg, dh, di, dj, dk, dl, dm, dn, do, dp, dq, dr, ds, dt, du, dv, dw, dx, dy))
+
+print ('-' * 29)
+print ('\033[0;30;46m-------BITOLA DOS FIOS-------\033[m')
+print ('-' * 29)
+print ('\033[36m{} polegada circular |{} kcmil, MCM |{} cmil |\033[m'.format(dz, ea, eb))
+
+print ('-' * 30)
+print ('\033[0;30;46m-----------NATURAIS-----------\033[m')
+print ('-' * 30)
+print ('\033[36m{} L² |\033[m'.format(ec))
+
+print ('\033[36mA CATEC agradece a sua consulta!\033[m')
