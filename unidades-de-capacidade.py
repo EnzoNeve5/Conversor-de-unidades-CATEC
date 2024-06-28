a9aa588f5a893016beb04498cfe52d6ecf61cbb1
EnzoNeve5 <111204969+EnzoNeve5@users.noreply.github.com>
2024-06-28 11:01:05 -0300

    Add files via upload

---
+++ unidades-de-capacidade.py
@@ -0,0 +1,409 @@
+n1 = float(input ('Digite um valor em \033[32ml\033[m: '))
+a = n1 / 100
+b = n1 / 10
+c = n1
+d = n1 * 10
+e = n1 * 100
+f = n1 * 1000
+g = n1 * 1000000
+h = n1 / 1000000000000
+i = n1 / 1000
+j = n1
+k = n1 * 1000
+l = n1 * 1000000
+
+m = n1 / 10000000000 * 8107
+n = n1 / 100000 * 629
+o = n1 / 10000 * 2642 
+p = n1 / 1000 * 1057
+q = n1 / 1000 * 2113
+r = n1 / 1000 * 8454
+s = n1 / 100 * 3381
+t = n1 / 10 * 2705
+u = n1 * 16231
+
+v = n1 / 1000000 *  8648
+w = n1 / 100000 * 2838
+x = n1 / 10000 * 1135
+y = n1 / 1000 * 227
+z = n1 / 10000 * 9081
+aa = n1 / 1000 * 1816
+
+ab = n1 / 1000 * 2113
+ac = n1 / 1000 * 4227
+ad = n1 / 1000 * 4227
+ae = n1 / 1000 * 6763
+af = n1 / 100 * 1691
+ag = n1 / 100 * 3381
+ah = n1 / 100 * 6763
+ai = n1 / 10 * 1353
+aj = n1 / 10 * 2705
+ak = n1 / 10 * 2705
+al = n1 * 16231
+
+au = n1 / 1000000 * 1427
+av = n1 / 100000 * 611
+aw = n1 / 10000 * 275
+ax = n1 / 100 * 11
+ay = n1 / 100 * 22
+az = n1 / 10000 * 8799
+ba = n1 / 100 * 176
+bb = n1 / 10 * 352
+bc = n1 / 10 * 2816
+bd = n1 / 10 * 8447
+be = n1 * 16894
+
+bf = n1 / 10000000000000000 * 2399
+bg = n1 / 100000000 * 3531
+bh = n1 / 1000000 * 1308
+bi = n1 / 10000000 * 3531
+bj = n1 / 100000 * 3531
+bk = n1 / 100 * 6102
+
+bl = n1 / 1000000 * 1047
+bm = n1 / 1000000 * 2095
+bn = n1 / 1000000 * 3142
+bo = n1 / 100000 * 419
+bp = n1 / 1000000 * 6285
+bq = n1 / 100000 * 838
+br = n1 / 100000 * 1466
+
+bs = n1 / 1000000 * 1018
+bt = n1 / 100000 * 611
+bu = n1 / 100000 * 1222
+bv = n1 / 100000 * 1222 * 2
+bw = n1 / 100000 * 1222 * 4
+
+bx = n1 / 1000000 * 8522
+by = n1 / 100000 * 1704
+bz = n1 / 100000 * 1704
+ca = n1 / 100000 * 3409
+
+cb = n1 / 1000 * 4227
+cc = n1 / 100 * 6763
+cd = n1 / 10 * 2029
+
+ce = n1 * 50
+cf = n1 * 100
+cg = n1 * 200
+
+ch = n1 / 1000 * 4167
+ci = n1 / 100 * 6667
+cj = n1 * 200
+
+ck = n1 * 236731775796070000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+
+cl = n1 / 100000000000000000000000000000000000000000000000000000000 * 3404
+cm = n1 / 1000000000000000000000000000000000000000000000000000000 * 1181
+cn = n1 / 10000000000000000000000000000000000000 * 1718
+co = n1 / 100000000000000000000000000000000 * 3711
+
+cp = n1 / 100000 * 14
+cq = n1 / 10000000 * 3531
+cr = n1 / 10000000 * 3531
+
+cs = n1 / 10000000000 * 4238
+ct = n1 / 10000000 * 2759
+cu = n1 / 10000000 * 4238
+cv = n1 / 10000000 * 5547
+cw = n1 / 10000000 * 8829
+cx = n1 / 1000 * 1
+cy = n1 / 100000 * 2774
+cz = n1 / 10000 * 4238
+
+da = n1 / 10 * 5544
+db = n1 / 100 * 5544
+dc = n1 / 1000 * 5544
+dd = n1 / 10000 * 5544
+de = n1 / 100000 * 5544
+df = n1 / 1000000 * 5544
+
+dg = n1 / 1000000 * 9657
+dh = n1 / 100000 * 1931
+di = n1 / 100000 * 9657
+dj = n1 / 10000 * 9657
+dk = n1 / 1000 * 9657
+dl = n1 / 100 * 9657
+
+dm = n1 / 100
+dn = n1 / 10
+do = n1
+dp = n1 * 10
+dq = n1 * 100
+dr = n1 * 1000
+
+ds = n1 * 5 / 10000
+dt = n1 * 4 / 100
+du = n1 * 5 / 100
+dv = n1 * 1
+dw = n1 * 8
+dx = n1 * 32
+dy = n1 * 128
+
+dz = n1 / 100000 * 1957
+ea = n1 / 10000 * 1174
+eb = n1 / 10000 * 2348
+ec = n1 / 10000 * 9393
+ed = n1 / 1000 * 1879
+ee = n1 / 1000 * 3757
+ef = n1 / 100 * 1503
+eg = n1 / 100 * 2254
+eh = n1 / 10 * 2254
+
+ei = n1 / 100000 * 2609
+ej = n1 / 100000 * 3914
+ek = n1 / 10000 * 3131
+el = n1 / 1000 * 1879
+em = n1 / 1000 * 3757
+en = n1 / 1000 * 7514
+eo = n1 / 100 * 1503
+ep = n1 / 100 * 2254
+eq = n1 / 100 * 4509
+er = n1 / 100 * 9017
+es = n1 / 10 * 1127
+et = n1 / 10 * 2254
+
+eu = n1 / 1000000 * 1928
+ev = n1 / 100000 * 3856
+ew = n1 / 100000 * 7712
+ex = n1 / 100000 * 7712
+ey = n1 / 10000 * 1157
+ez = n1 / 10000 * 2314
+fa = n1 / 10000 * 3085
+fb = n1 / 1000 * 1851
+fc = n1 / 1000 * 3702
+fd = n1 / 1000 * 3702
+fe = n1 / 1000 * 5552
+ff = n1 / 1000 * 7403
+fg = n1 / 10 * 111
+fh = n1 / 100 * 1481
+fi = n1 / 100 * 2221
+fj = n1 / 100 * 8884
+
+fk = n1 / 1000000 * 1502
+fl = n1 / 100000 * 1802
+fm = n1 / 100000 * 7209
+fn = n1 / 10000 * 2163
+fo = n1 / 10000 * 4325
+fp = n1 / 10000 * 8651
+fq = n1 / 10000 * 2163
+
+fr = n1 / 1000000 * 3875
+fs = n1 / 1000 * 62
+ft = n1 / 1000 * 62
+fu = n1 / 1000 * 496
+fv = n1 / 1000 * 1323
+fw = n1 / 1000 * 1984
+fx = n1 / 1000 * 7937
+fy = n1 / 1000 * 7943
+fz = n1 / 1000 * 7937
+
+ga = n1 / 1000000 * 1208
+gb = n1 / 100000 * 1812
+gc = n1 / 100000 * 7246
+gd = n1 / 10000 * 2899
+ge = n1 / 10000 * 5797
+gf = n1 / 1000 * 1159
+gg = n1 / 1000 * 2319
+gh = n1 / 1000 * 4638
+gi = n1 / 1000 * 9275
+
+gj = n1 / 100000 * 119
+gk = n1 / 1000000 * 2381
+gl = n1 / 100000 * 5952
+gm = n1 / 100000 * 5952
+gn = n1 / 1000 * 119
+go = n1 / 10000 * 7143
+gp = n1 / 1000 * 2857
+gq = n1 / 1000 * 5714
+gr = n1 / 100 * 1143
+
+gs = n1 / 100000 * 2941
+gt = n1 / 1000 * 1203
+
+gu = n1 / 100000 * 3811
+gv = n1 / 10000 * 813
+gw = n1 / 10000 * 3049
+
+gx = n1 / 10000 * 813
+gy = n1 / 1000 * 813
+gz = n1 / 10000 * 3252
+ha = n1 / 1000 * 1301
+hb = n1 / 1000 * 1626
+hc = n1 / 100 * 813
+hd = n1 / 100 * 1626
+
+he = n1 / 1000000 * 4545
+hf = n1 / 1000000 * 9091
+hg = n1 / 100000 * 4545
+hh = n1 / 10000 * 1364
+hi = n1 / 10000 * 8182
+hj = n1 / 1000 * 3273
+hk = n1 / 100 * 1964
+hl = n1 / 100 * 3927
+
+hm = n1 / 100000 * 4545
+hn = n1 / 10000 * 2727
+ho = n1 / 1000 * 3273
+
+print ('-' * 30)
+print ('\033[0;30;46m-------MEDIDAS MÉTRICAS-------\033[m')
+print ('-' * 30)
+print ('\033[36m{} hl |{} decalitro |{} l |{} dl |{} cl |{} ml |{} µl |{} km³ |{} m³ |{} dm³ |{} cc |{} mm³ |\033[m'.format(a, b, c, d, e, f, g, h, i, j, k, l))
+
+print ('-' * 30)
+print ('\033[0;30;46m---------EUA LÍQUIDOS---------\033[m')
+print ('-' * 30)
+print ('\033[36m{} acre-pé |{} barril (petróleo) |{} gal |{} qt |{} pt |{} gill |{} fl oz |{} fl dr |{} min |\033[m'.format(m, n, o, p, q, r, s, t, u))
+
+print ('-' * 29)
+print ('\033[0;30;46m----------EUA SECOS----------\033[m')
+print ('-' * 29)
+print ('\033[36m{} barril |{} bu |{} pk |{} gal |{} qt |{} pt |\033[m'.format(v, w, x, y, z, aa))
+
+print ('-' * 30)
+print ('\033[0;30;46m---------APOTHECARIES---------\033[m')
+print ('-' * 30)
+print ('\033[36m{} pt |{} copo de vidro |{} xícara de café da manhã |{} xícara de chá |{} taça de vinho |{} ƒ℥ |{} colher de sopa |{} colher de sobremesa |{} ƒʒ |{} colher de chá |{} minim |\033[m'.format(ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al))
+
+print ('-' * 30)
+print ('\033[0;30;46m----------INGLATERRA----------\033[m')
+print ('-' * 30)
+print ('\033[36m{} perch |{} barril |{} bu |{} pk |{} gal |{} qt |{} pt |{} oz |{} fl dr |{} escrópulo líquido |{} min |\033[m'.format(au, av, aw, ax, ay, az, ba, bb, bc, bd, be))
+
+print ('-' * 30)
+print ('\033[0;30;46m--------EUA/INGLATERRA--------\033[m')
+print ('-' * 30)
+print ('\033[36m{} mi³ |{} mcf |{} yd³ |{} ccf |{} ft³ |{} in³ |\033[m'.format(bf, bg, bh, bi, bj, bk))
+
+print ('-' * 30)
+print ('\033[0;30;46m-------INGLATERRA|VINHO-------\033[m')
+print ('-' * 30)
+print ('\033[36m{} tun |{} butt |{} puncheon |{} hogsheads |{} tierce |{} barril |{} rundlet |\033[m'.format(bl, bm, bn, bo, bp, bq, br))
+
+print ('-' * 30)
+print ('\033[0;30;46m------INGLATERRA|CERVEJA------\033[m')
+print ('-' * 30)
+print ('\033[36m{} hogsheads |{} barril |{} kilderkin |{} firkin |{} pin |\033[m'.format(bs, bt, bu, bv, bw))
+
+print ('-' * 29)
+print ('\033[0;30;46m---------EUA|CERVEJA---------\033[m')
+print ('-' * 29)
+print ('\033[36m{} Barril de cerveja norte-americano |{} Meio-barril de cerveja norte-americano |{} Barrilete de cerveja norte-americano (típico) |{} Quarto de barril de cerveja norte-americano |\033[m'.format(bx, by, bz, ca))
+
+print ('-' * 29)
+print ('\033[0;30;46m--------EUA~CULINÁRIA--------\033[m')
+print ('-' * 29)
+print ('\033[36m{} xícara |{} colher de sopa |{} colher de chá |\033[m'.format(cb, cc, cd))
+
+print ('-' * 29)
+print ('\033[0;30;46m-----AUSTRÁLIA~CULINÁRIA-----\033[m')
+print ('-' * 29)
+print ('\033[36m{} colher de sopa |{} colher de sobremesa |{} colher de chá |\033[m'.format(ce, cf, cg))
+
+print ('-' * 29)
+print ('\033[0;30;46m----------CULINÁRIA----------\033[m')
+print ('-' * 29)
+print ('\033[36m{} xícara |{} colher de sopa |{} colher de chá |\033[m'.format(ch, ci, cj))
+
+print ('-' * 29)
+print ('\033[0;30;46m------UNIDADES NATURAIS------\033[m')
+print ('-' * 29)
+print ('\033[36m{} L³ |\033[m'.format(ck))
+
+print ('-' * 30)
+print ('\033[0;30;46m----------ASTRONOMIA----------\033[m')
+print ('-' * 30)
+print ('\033[36m{} pc³ |{} ano-luz cúbico |{} minuto-luz cúbico |{} segundo-luz cúbico |\033[m'.format(cl, cm, cn, co))
+
+print ('-' * 29)
+print ('\033[0;30;46m------------NAVAL------------\033[m')
+print ('-' * 29)
+print ('\033[36m{} arqueação |{} tonelada de registro |{} Sistema de Medição Universal do Canal do Panamá |\033[m'.format(cp, cq, cr))
+
+print ('-' * 29)
+print ('\033[0;30;46m-----------MADEIRA-----------\033[m')
+print ('-' * 29)
+print ('\033[36m{} milhão de board-feet (mmfbm, mmbdft, mmbf) |{} corda |{} mil board-feet (mfbm, mbdft, mbf) |{} HT |{} tonelada cúbica de madeira |{} estere |{} hoppus pé, hoppus ft³ (h ft) |{} board-foot (fbmm, bdft, bf) |\033[m'.format(cs, ct, cu, cv, cw, cx, cy, cz))
+
+print ('-' * 29)
+print ('\033[0;30;46m------------JAPÃO------------\033[m')
+print ('-' * 29)
+print ('\033[36m{} sai |{} shaku |{} go |{} sho |{} to |{} koku |\033[m'.format(da, db, dc, dd, de, df))
+
+print ('-' * 29)
+print ('\033[0;30;46m-------CHINA 1915#1930-------\033[m')
+print ('-' * 29)
+print ('\033[36m{} dan |{} hu |{} dou |{} sheng |{} ge |{} shao |\033[m'.format(dg, dh, di, dj, dk, dl))
+
+print ('-' * 29)
+print ('\033[0;30;46m-------CHINA 1930#HOJE-------\033[m')
+print ('-' * 29)
+print ('\033[36m{} dan |{} dou |{} sheng |{} ge |{} shao |{} cuo |\033[m'.format(dm, dn, do, dp, dq, dr))
+
+print ('-' * 29)
+print ('\033[0;30;46m----------TAILÂNDIA----------\033[m')
+print ('-' * 29)
+print ('\033[36m{} kwian |{} sat |{} thang |{} thanan |{} fai mue |{} kam mue |{} yip mue |\033[m'.format(ds, dt, du, dv, dw, dx, dy))
+
+print ('-' * 29)
+print ('\033[0;30;43m------GRÉCIA ÁTICO SECO------\033[m')
+print ('-' * 29)
+print ('\033[34m{} medimnos |{} hekteus |{} hēmiekton |{} choinix |{} xestēs |{} kotylē ou hēmina |{} oxybathon |{} kyathos |{} kochliarion |\033[m'.format(dz, ea, eb, ec, ed, ee, ef, eg, eh))
+
+print ('-' * 30)
+print ('\033[0;30;43m-----GRÉCIA ÁTICO LÍQUIDO-----\033[m')
+print ('-' * 30)
+print ('\033[34m{} metrētēs |{} keramion |{} chous |{} xestēs |{} kotylē, tryblion ou hēmina |{} tetarton, hēmikotylē |{} oxybathon |{} kyathos |{} konchē |{} mystron |{} chēmē |{} kochliarion |\033[m'.format(ei, ej, ek, el, em, en, eo, ep, eq, er, es, et))
+
+print ('-' * 30)
+print ('\033[0;30;43m-------------ROMA-------------\033[m')
+print ('-' * 30)
+print ('\033[34m{} culeus |{} amphora quadrantal |{} urna |{} modius castrensis |{} modius |{} semimodius |{} congius |{} sextarius |{} hemina |{} cotyla |{} triens |{} quartarius |{} sextans |{} acetabulum |{} cyathus |{} ligula |\033[m'.format(eu, ev, ew, ex, ey, ez, fa, fb, fc, fd, fe, ff, fg, fh, fi, fj))
+
+print ('-' * 29)
+print ('\033[0;30;43m-----ESPANHA ANTIGA SECO-----\033[m')
+print ('-' * 29)
+print ('\033[34m{} cahíz |{} fanega |{} cuartilla |{} celemín |{} medio |{} cuartillo |{} ochavo |\033[m'.format(fk, fl, fm, fn, fo, fp, fq))
+
+print ('-' * 30)
+print ('\033[0;30;43m----ESPANHA ANTIGA LÍQUIDO----\033[m')
+print ('-' * 30)
+print ('\033[34m{} moyo |{} cántara |{} arroba |{} azumbre |{} botella |{} cuartillo |{} copa |{} panilla |{} cortadillo |\033[m'.format(fr, fs, ft, fu, fv, fw, fx, fy, fz))
+
+print ('-' * 30)
+print ('\033[0;30;43m-----PORTUGAL ANTIGO SECO-----\033[m')
+print ('-' * 30)
+print ('\033[34m{} moio |{} fanga |{} alqueire |{} quarta |{} oitava |{} maquia |{} selamim |{} meio-selamim |{} quarto de selamim |\033[m'.format(ga, gb, gc, gd, ge, gf, gg, gh, gi))
+
+print ('-' * 29)
+print ('\033[0;30;43m---PORTUGAL ANTIGO LÍQUIDO---\033[m')
+print ('-' * 29)
+print ('\033[34m{} tonel |{} pipa |{} almude |{} cântaro |{} pote |{} canada |{} quartilho |{} meio-quartilho |{} quarto de quartilho |\033[m'.format(gj, gk, gl, gm, gn, go, gp, gq, gr))
+
+print ('-' * 30)
+print ('\033[0;30;43m--------FRANCÊS ANTIGO--------\033[m')
+print ('-' * 30)
+print ('\033[34m{} minot |{} litron |\033[m'.format(gs, gt))
+
+print ('-' * 30)
+print ('\033[0;30;43m------RÚSSIA ANTIGA SECA------\033[m')
+print ('-' * 30)
+print ('\033[34m{} cetverik |{} vedro |{} garnetz |\033[m'.format(gu, gv, gw))
+
+print ('-' * 29)
+print ('\033[0;30;43m----RÚSSIA ANTIGA LÍQUIDA----\033[m')
+print ('-' * 29)
+print ('\033[34m{} vedro |{} shtoff |{} chetvert |{} garrafa de vinho |{} garrafa de vodca |{} charka |{} shkalik |\033[m'.format(gx, gy, gz, ha, hb, hc, hd))
+
+print ('-' * 29)
+print ('\033[0;30;42m---------BÍBLIA SECO---------\033[m')
+print ('-' * 29)
+print ('\033[35m{} coro |{} letec |{} efa |{} seá |{} cabe |{} log |{} bezah |{} kezayit |\033[m'.format(he, hf, hg, hh, hi, hj, hk, hl))
+
+print ('-' * 30)
+print ('\033[0;30;42m--------BÍBLIA LÍQUIDO--------\033[m')
+print ('-' * 30)
+print ('\033[35m{} bato |{} him |{} log |\033[m'.format(hm, hn, ho))
+
+print ('\033[36mA CATEC agradece a sua consulta!\033[m')
